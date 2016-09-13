cae775a0b4838dcba3175ab2319a2f29451f36b0
coder187 <qwerty78>
2016-09-13 18:24:05 +0100

    Second Commit

---
+++ ClassExample/SystemUser.vb
@@ -0,0 +1,71 @@
+﻿Public Class SystemUser
+    Public Property id() As String
+        Get
+            Return m_id
+        End Get
+        Set(value As String)
+            m_id = Value
+        End Set
+    End Property
+    Private m_id As String
+
+    Public Property first_name() As String
+        Get
+            Return m_first_name
+        End Get
+        Set(value As String)
+            m_first_name = Value
+        End Set
+    End Property
+    Private m_first_name As String
+
+    Public Property last_name() As String
+        Get
+            Return m_last_name
+        End Get
+        Set(value As String)
+            m_last_name = value
+        End Set
+    End Property
+    Private m_last_name As String
+
+    Public Property link() As String
+        Get
+            Return m_link
+        End Get
+        Set(value As String)
+            m_link = value
+        End Set
+    End Property
+    Private m_link As String
+
+    Public Property username() As String
+        Get
+            Return m_username
+        End Get
+        Set(value As String)
+            m_username = value
+        End Set
+    End Property
+    Private m_username As String
+
+    Public Property gender() As String
+        Get
+            Return m_gender
+        End Get
+        Set(value As String)
+            m_gender = value
+        End Set
+    End Property
+    Private m_gender As String
+
+    Public Property locale() As String
+        Get
+            Return m_locale
+        End Get
+        Set(value As String)
+            m_locale = value
+        End Set
+    End Property
+    Private m_locale As String
+End Class
---
+++ ClassExample/WebForm1.aspx.vb
@@ -0,0 +1,17 @@
+﻿Public Class WebForm1
+    Inherits System.Web.UI.Page
+
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        Dim Users As New List(Of SystemUser)()
+
+        Users.Add(New SystemUser() With {.id = "biglongalphanumericstring_!%$345^", .first_name = "Jonathan", .link = "jkelly@ksd.ie", .locale = "IE", .username = "jkelly"})
+        Users.Add(New SystemUser() With {.id = "biglongalphanumericstring_!%$678^", .first_name = "Mike", .link = "Mkelly@msn.com", .locale = "IE", .username = "mkelly"})
+        Users.Add(New SystemUser() With {.id = "biglongalphanumericstring__^&*(1", .first_name = "Sponge", .link = "sbob@bikinibottom.co.bb", .locale = "bb", .username = "sbob109"})
+
+        GV_users.DataSource = Users
+        GV_users.DataBind()
+        Response.Write("All Done")
+    End Sub
+
+End Class
